07d170b1634229b4602eb745dc8584310fe543c9
Isaac Antonio Gutierrez <Inggutierrez1113@hotmail.com>
2016-12-08 14:00:24 -0400

    add some logic

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 
 
-var Componentes = {
-	valor:0,
-	incrementar:function(){
-      Componentes.valor++;
-     console.log(Componentes.valor);
-	},
-	reducir:function(){
-		if(Componentes.valor==0)
-			return;
-   Componentes.valor--;
-   console.log(Componentes.valor);
+(function(){
+	//main function
+	 startSymulation();
+}())
+
+
+function startSymulation(){
+	document.body.style.width = "100%";
+	document.body.style.height = "100%";
+	var node = document.createElement("DIV");
+	node.style.height ="10em";
+	node.style.width ="10em";
+	node.style.backgroundColor ="red"
+	node.style.borderRadius = "100%";
+	node.style.position = "absolute";
+	
+	document.body.appendChild(node);
+
+	document.body.onmouseover = function(event){
+		
+        node.style.left = event.clientX;
+        node.style.top = event.clientY;
 	}
+
 }
 
-
-
+ 
